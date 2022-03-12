e5472198b1771b4bb878417c3c7b30e99bd9bca5
Drashtee <drashteeparmar@gmail.com>
2022-03-12 12:04:39 -0500

    setting up connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+    // useFindAndModify: false,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
+})
+// mongoose.set("debug", true);
+
+// app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(` Connected  on http://localhost:${PORT}`));
