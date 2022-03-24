24749c3813621f56f8bae4bfbe9c8feb64b09763
Drashtee <drashteeparmar@gmail.com>
2022-03-24 19:07:20 -0400

    updated server and model

---
+++ models/Thoughts.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText:
+            {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280
+            },
+        createdAt:
+            {
+                type: Date,
+                default: Date.now
+            },
+        username:
+            {
+                type: String,
+                required: true
+            },
+        reactions: [ReactionSchema]
+
+    },
+    {
+        toJSON:
+            {
+                virtuals: true
+
+            },
+        id: false
+    });
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
-    // useFindAndModify: false,
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 }, err => {
     if(err) throw err;
     console.log('Connected to MongoDB!!!')
 })
-// mongoose.set("debug", true);
+mongoose.set("debug", true);
 
-// app.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(` Connected  on http://localhost:${PORT}`));
